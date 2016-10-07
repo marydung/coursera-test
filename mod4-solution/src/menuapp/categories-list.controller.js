060e66ae682830114cdbf54a9218e1c83417ac41
Mary Dung <marydung@outlook.com>
2016-10-07 17:38:52 -0500

    remove MenuService from categories-list.controller

--- mod4-solution/src/menuapp/categories-list.controller.js
+++ mod4-solution/src/menuapp/categories-list.controller.js
@@ -5,8 +5,8 @@ angular.module('MenuApp')
 .controller('CategoriesListController', CategoriesListController);
 
 
-CategoriesListController.$inject = ['MenuDataService', 'items'];
-function CategoriesListController(MenuDataService, items) {
+CategoriesListController.$inject = ['items'];
+function CategoriesListController(items) {
   var catCtrl = this;
   catCtrl.items = items;
   //console.log("catCtrl.items="+catCtrl.items);
